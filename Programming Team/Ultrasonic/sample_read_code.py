36fa8476881f8dd475229f34e38f237e231dbca0
Sahil Kapur <sahilkapur.a@gmail.com>
2018-10-23 22:18:04 -0400

    added code for sample ultrasonic sensor

--- Programming Team/Ultrasonic/sample_read_code.py
+++ Programming Team/Ultrasonic/sample_read_code.py
@@ -12,3 +12,19 @@ def init():
 def read_value(dm):
     dist = sensor.measure_distance(dm)
     return dist
+    
+left_front_dist = 0
+right_front_dist = 0
+
+front_dist = 0
+back_dist = 0
+
+left_back_dist = 0
+right_back_front_dist = 0
+
+previous = []
+current = [left_front_dist, right_front_dist,
+
+if (left_front_dist < 20 and front_dist < 5):
+	#stop
+	break
