63627713a9efd3ec4e6077edda56698d0363fe5f
CheersYan <18019128998@163.com>
2018-10-23 22:25:48 -0400

    added the new comments and 2 dimensional array for ultrasonic

--- Programming Team/Ultrasonic/sample_read_code.py
+++ Programming Team/Ultrasonic/sample_read_code.py
@@ -23,7 +23,15 @@ left_back_dist = 0
 right_back_front_dist = 0
 
 previous = []
-current = [left_front_dist, right_front_dist,
+current = [left_front_dist, right_front_dist, front_dist, back_dist, left_back_dist, right_back_front_dist]
+#0 is left front, 1 is right front, 2 is front, 3 is back, 4 is left back, 5 is right back
+previous[0] = current; #0 is the most recent and 5 is the oldest data.
+def storeNew():
+	for x in xrange(5,1):
+		previous[x] = previous[x-1];
+		pass
+	previous[0] = current;
+
 
 if (left_front_dist < 20 and front_dist < 5):
 	#stop
